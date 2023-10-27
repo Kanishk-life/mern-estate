9b43f624f6d25667cc2e13a2b591b501fffe5759
Kanishk-life <kanishkbhatt1823@gmail.com>
2023-10-27 16:54:22 +0530

    sign in updated page

--- client/src/assets/pages/Signin.jsx
+++ client/src/assets/pages/Signin.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import {Link,useNavigate} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { signInStart,signInSuccess,signInFailure } from '../../redux/user/userSlice.js';
+import { signInStart,signInSuccess,signInFailure } from '../../redux/user/userSlice';
 
 
 export default function SignIn() {
@@ -29,6 +29,7 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
+      console.log(data);
       if(data.success === false){
         dispatch(signInFailure(data.message));
         return;
